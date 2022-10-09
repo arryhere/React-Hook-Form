503eac2ba5faa7fdddfdc0714b4e71af6e6a11d4
Arijit <arijitdasofficial@gmail.com>
2022-10-10 03:31:14 +0530

    feat: file handling

--- src/Components/Form/FileHandling.jsx
+++ src/Components/Form/FileHandling.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function FileHandling() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <div>FileHandling</div>
+      <section>
+        <form onSubmit={handleSubmit(onSubmit)} className="container">
+          <input
+            multiple  // for multiple input
+            type="file"
+            id="image"
+            {...register('image', {
+              required: 'Image is required',
+            })}
+          />
+          <p className="error-msg">{errors?.image?.message}</p>
+
+          <input type="submit" defaultValue={'Submit'} className="btn" />
+          <input
+            onClick={() => reset()}
+            type="button"
+            defaultValue={'Reset'}
+            className="btn"
+          />
+        </form>
+      </section>
     </>
   );
 }
